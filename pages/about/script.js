6a2791c8be1b036fe5e87d71c79bb12306902f60
Analyze_ <101422486+Analyze0@users.noreply.github.com>
2024-01-09 09:10:07 -0500

    Create script.js

---
+++ pages/about/script.js
@@ -0,0 +1,17 @@
+let viewportWidth = window.innerWidth;
+let e = 1 / (viewportWidth / 5);
+
+setInterval(function() {
+  e--;
+  document.getElementById('crawl').style.marginTop = ""+e+"px";
+}, 15);
+
+window.onload = function() {
+  setTimeout(function() {
+    window.scrollTo(0, 0);
+  }, 1);
+};
+
+document.getElementById('back').onmousedown = function(e){
+  window.location.href = "/index.html";
+}
