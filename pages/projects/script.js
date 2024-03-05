c3a176e90f98ad196e84fc2f19e73d45afec6583
Quingle <101422486+Analyze0@users.noreply.github.com>
2024-03-05 06:47:07 -0500

    Update script.js

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -24,38 +24,40 @@ window.onscroll = function(e) {
     document.getElementById('tv').src = "images/ruler.png";
   }
   else if (scrollVal > 300 && scrollVal <= 400) {
-    if (project != "popcatCookieClicker") {
+    if (project != "nit") {
       document.querySelector('.description').style.top = "100%";
       setTimeout(function() {
         document.querySelector('.description').innerHTML = `
-        <h1>Popcat + Cookie Clicker</h1>
+        <h1>NIT (Nebraskan Institute of Thermometers)</h1>
         <center>
-        <p>This is a game that I developed a while back. It is basically the popcat website, except it is also like cookie clicker, where you can buy upgrades. Sadly, I stopped working on this project a while ago.</p>
+        <p>This is a fun little project that I made with a couple friends where we basically just ripped off the MIT website (no copy and paste) and added some funny skits and stuff to it.</p><br>
+        <a src="https://analyze0.github.io/nit/">Link</a>
         </center>
         `;
         document.querySelector('.description').style.top = "5px";
       }, 1000);
     }
-    project = "popcatCookieClicker";
-    projectTitle.innerHTML = "Popcat + Cookie Clicker";
-    document.getElementById('tv').src = "images/popcat.png";
+    project = "nit";
+    projectTitle.innerHTML = "NIT (Nebraskan Institute of Thermometers)";
+    document.getElementById('tv').src = "images/nit.png";
   }
   else if (scrollVal > 200 && scrollVal <= 300) {
-    if (project != "gameProject") {
+    if (project != "worldwideStudent") {
       document.querySelector('.description').style.top = "100%";
       setTimeout(function() {
         document.querySelector('.description').innerHTML = `
-        <h1>Game Project</h1>
+        <h1>Worldwide Student</h1>
         <center>
-      <p>I've recently started working on this game project. It's going to be really exciting!</p>
+      <p>I've recently started working on this game project. It's going to be really exciting!</p><br>
+      <a src="worldwide-student.vercel.app">Link</a>
       </center>
         `;
         document.querySelector('.description').style.top = "5px";
       }, 1000);
     }
-    project = "gameProject";
-    projectTitle.innerHTML = "Game Project";
-    document.getElementById('tv').src = "images/gameProject.png";
+    project = "worldwideStudent";
+    projectTitle.innerHTML = "Worldwide Student";
+    document.getElementById('tv').src = "images/worldwideStudent.png";
   }
   else if (scrollVal > 500) {
     if (project != "futureProject") {
